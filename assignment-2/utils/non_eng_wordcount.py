9e8eabf0e41dd225541efc06b2853cbdd32f6fd4
Dev Parikh <dev.dpparikh@gmail.com>
2024-10-03 15:50:33 -0400

    Assignment-2 complete push
    Assignment-2 complete push

---
+++ assignment-2/utils/non_eng_wordcount.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+import re
+import enchant
+
+class NonEngWordCount(MRJob):
+
+    def __init__(self, *args, **kwargs):
+        super(NonEngWordCount, self).__init__(*args, **kwargs)
+        self.d = enchant.Dict("en_US")
+
+    def mapper(self, _, line):
+        for word in line.split():
+            if word:
+                formatted_word = ''.join(letter for letter in word.lower() if letter.isalnum()).strip() 
+                if formatted_word and not self.d.check(formatted_word):
+                    yield(formatted_word, 1)
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+if __name__ == '__main__':
+    NonEngWordCount.run()
---
+++ assignment-2/utils/wordcount.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+class WordCountJob(MRJob):
+     def mapper(self, _, line):
+         for word in line.split():
+            if word:
+                formatted_word = ''.join(letter for letter in word.lower() if letter.isalnum()).strip()
+                yield(formatted_word, 1)
+
+     def reducer(self, word, counts):
+         yield(word, sum(counts))
+
+
+if __name__ == '__main__':
+    WordCountJob.run()
